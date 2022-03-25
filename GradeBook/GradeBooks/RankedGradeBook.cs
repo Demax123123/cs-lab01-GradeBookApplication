1397d5a2935ee20ed6fa2a525f9325fe29061c4e
Demax123123 <danielwlodek522@gmail.com>
2022-03-25 23:59:57 +0100

    1

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,66 @@
+﻿
+using GradeBook.Enums;
+using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var jednaPiataUczniow = (int)Math.Ceiling(Students.Count * 0.2);
+            var oceny = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (oceny[jednaPiataUczniow - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (oceny[(jednaPiataUczniow * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (oceny[(jednaPiataUczniow * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (oceny[(jednaPiataUczniow * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+            public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
